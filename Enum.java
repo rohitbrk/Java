1820ee65acadad49dde5dbf44d8db5e062dd99e2
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-10-12 07:07:32 +0530

    Create Enum.java

---
+++ Enum.java
@@ -0,0 +1,17 @@
+enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
+public class Enum {
+    public static void main(String[] args) {
+        Day day1;
+        day1 = Day.SUNDAY;
+        System.out.println(day1);
+    }
+}
